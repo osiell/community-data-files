3083353af6f2207acafacb0be916d367e2809421
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2017-10-09 16:47:02 +0200

    [IMP] l10n_eu_nace: import naces before changing partner categories

--- l10n_eu_nace/migrations/8.0.2.2.0/post-migrate.py
+++ l10n_eu_nace/migrations/8.0.2.2.0/post-migrate.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import openerp
 
 from openerp import api, SUPERUSER_ID
 
@@ -8,10 +9,24 @@ logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    fix_nace_id(env)
+    if version:
+        actions = [import_nace, fix_nace_id]
+        for action in actions:
+            with openerp.api.Environment.manage():
+                with openerp.registry(env.cr.dbname).cursor() as new_cr:
+                    new_env = openerp.api.Environment(new_cr, env.uid, env.context)
+                    action(new_env)
+
+def import_nace(env):
+    logger.info("[IMPORT res.partner.nace] download from ramon european service")
+    wiz_model = env['nace.import']
+    vals = wiz_model.default_get(wiz_model._fields)
+    wiz = wiz_model.create(vals)
+    wiz.run_import()
 
 def fix_nace_id(env):
     logger.info("[UDPATE res.partner] Correction category_id for nace")
+
     data_records = env['ir.model.data'].search([
         ('model', '=', 'res.partner.category'),
         ('res_id', '!=', False),
@@ -26,21 +41,18 @@ def fix_nace_id(env):
     res_partners = env['res.partner'].search([
         '|', ('active', '=', True), ('active', '=', False),
         ('category_id', '!=', False),
-        #('category_id', 'in', nace_categ_ids)
-        ('category_id', 'child_of', nace_categ_ids)
+        ('category_id', 'in', nace_categ_ids)
         ])
 
     for partner in res_partners:
-        # if re.match("^\[.*\]", category.name):
-        #     nace_code = category.name.split("[")[1].split("]")[0]
         category_ids = category_model.browse()
         for category in partner.category_id:
             if category in nace_categs:
-                if re.match("^\[.*\]", category.parent_id.name):
-                    nace_code = category.parent_id.name.split("[")[1].split("]")[0]
-                    nace = session.env['res.partner.nace'].search([('code', '=', nace_code)])
+                if re.match("^\[.*\]", category.name):
+                    nace_code = category.name.split("[")[1].split("]")[0]
+                    nace = env['res.partner.nace'].search([('code', '=', nace_code)])
                     nace.ensure_one()
                     partner.nace_ids |= nace
-                    category_ids += category
-                    category.active= False
-    data_records.unlink()
+                    #category_ids += category
+                    #category.active= False
+    #data_records.unlink()
